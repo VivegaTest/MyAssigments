dd4a8c74d9958e1136a534d8f5d823c529b3b84e
VivegaTest <107422141+VivegaTest@users.noreply.github.com>
2024-04-06 04:54:38 +0100

    Add files via upload

---
+++ Day1_Assignment/Assignment.js
@@ -0,0 +1,26 @@
+const browser_Version = 'Chrome';
+function getBrowserVersion(){
+  if(browser_Version ==='Chrome'){
+    var browserVersion = "Chrome";
+  }
+  else{
+    console.log("browser version is not matched");
+  }
+  console.log(browserVersion);
+}
+
+getBrowserVersion();
+
+
+
+
+let firstName="Vivega";
+console.log( typeof firstName);
+let companyname="Veeva Systems";
+console.log( typeof companyname);
+let mobileNumber="011111";
+console.log( typeof mobileNumber);
+let isAutomation=true;
+console.log( typeof isAutomation);
+let hasPlawright;
+console.log(typeof hasPlawright);
