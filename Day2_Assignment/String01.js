37b8a2f4c2b9b551d27ae7cd859f2a9c95950027
VivegaTest <107422141+VivegaTest@users.noreply.github.com>
2024-04-09 19:46:07 +0100

    Update String01.js

--- Day2_Assignment/String01.js
+++ Day2_Assignment/String01.js
@@ -1,15 +1,14 @@
 
 
-
-function reverse(input){
+function isPalindrome(input){
     let reverse = "";
-    let chars = input.split("");
-    let length = chars.size;
-    for(let index=chars.length-1;index>=0;index--){
-        reverse = reverse + chars[index];
+    let chars = input.trim().split("");
+    let length = chars.length;
+    for(let i=chars.length-1;i>=0;i--){
+        reverse = reverse + chars[i];
     }
     console.log("Reversed string is: " + reverse);
    return(input===reverse);
 }
 
-console.log(reverse("palindrome"));
+console.log(isPalindrome("palindrome"));
